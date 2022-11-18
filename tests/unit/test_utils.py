80ac22bf6acfecf958c7b67292d6284fd2621667
patrickryan <theyoungsoul@gmail.com>
2022-11-18 15:56:22 -0600

    WIP

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -3,6 +3,9 @@ from src.utils import to_upper, utc_to_central, to_lower
 def test_to_upper():
     assert "FOOBAR" == to_upper("foobar")
 
+def test_to_upper2():
+    assert "WHAT!" == to_upper("wHat!")
+
 def test_utc1():
     assert utc_to_central() is not None
 
